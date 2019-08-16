84da74de90858e0284103a49272d566f56cad23c
Khusni Jafar <khusnijafar@gmail.com>
2019-08-16 10:15:38 +0700

    add remote mysql

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || 'remotemysql.com',
+    user: process.env.DB_USER || 'hucrYvdCpN',
+    password: process.env.DB_PASS || 'sOF03EnqCt',
+    database: process.env.DB_NAME || 'hucrYvdCpN'
 })
 
 connection.connect((err) => {
